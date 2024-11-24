a912e6d3a3d7e5dc1a1b21c1c97c3af5fc16ff53
Gosun <28876168+frankol@users.noreply.github.com>
2024-11-24 11:27:31 +0100

    Update commands.h
    add difference between parallel advanced start/stop temperature  and the start temperature of alternative and parallel mode

--- HeishaMon/commands.h
+++ HeishaMon/commands.h
@@ -62,7 +62,8 @@ unsigned int set_heatingoffoutdoortemp(char *msg, unsigned char *cmd, char *log_
 unsigned int set_gpio16state(char *msg, unsigned char *cmd, char *log_msg);
 unsigned int set_bivalent_mode(char *msg, unsigned char *cmd, char *log_msg);
 unsigned int set_bivalent_start_temperature(char *msg, unsigned char *cmd, char *log_msg);
-unsigned int set_bivalent_stop_temperature(char *msg, unsigned char *cmd, char *log_msg);
+unsigned int set_bivalent_start_adv_temperature(char *msg, unsigned char *cmd, char *log_msg);
+unsigned int set_bivalent_stop_adv_temperature(char *msg, unsigned char *cmd, char *log_msg);
 
 //optional pcb commands
 unsigned int set_heat_cool_mode(char *msg, char *log_msg);
@@ -118,8 +119,10 @@ const cmdStruct commands[] PROGMEM = {
   { "SetBivalentMode", set_bivalent_mode },
   // bivalent start temp -  set from -15C to 35C 
   { "SetBivalentStartTemperature", set_bivalent_start_temperature },
-  // bivalent stop temp -  set from -15C to 35C
-  { "SetBivalentStopTemperature", set_bivalent_stop_temperature },
+  // bivalent parallel advanced start temp -  set from 0C to -15C 
+  { "SetBivalentStartAdvTemperature", set_bivalent_start_adv_temperature },
+  // bivalent parallel advanced stop temp -  set from 0C to -15C
+  { "SetBivalentStopAdvTemperature", set_bivalent_stop_adv_temperature },
   // set DHW temperature by sending desired temperature between 40C-75C
   { "SetDHWTemp", set_DHW_temp },
   // set heat/cool curves on z1 and z2 using a json input
